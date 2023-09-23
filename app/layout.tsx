3d9ba58c405edbc1961d5dea02826a6c0fd319b2
Suman Sigdel <sumansid1113@gmail.com>
2023-09-23 16:45:50 -0700

    fix docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SwiftCn UI",
+  title: "Swiftcn UI",
   description: "Build beautiful iOS apps with SwiftUI components",
 };
 
